f3413309a70f7b8b37ac6f46f82736ce605e8fae
yongde <yongdetan@gmail.com>
2026-02-01 12:50:36 +0800

    refactor: post entity and updated unit tests

--- src/main/java/com/yongde/blog/entity/Post.java
+++ src/main/java/com/yongde/blog/entity/Post.java
@@ -16,8 +16,10 @@ public class Post {
     @GeneratedValue
     private Long id;
 
+    @Column(nullable = false)
     private String title;
 
+    @Column(nullable = false)
     private String content;
 
     private String category;
