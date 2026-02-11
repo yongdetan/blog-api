435aab26ec55daa2100b5464f692a4b070db9077
Yong De <61530179+yongdetan@users.noreply.github.com>
2026-02-11 16:43:12 +0800

    test: add unit tests for UserRepository

--- src/main/java/com/yongde/blog/repository/UserRepository.java
+++ src/main/java/com/yongde/blog/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.yongde.blog.repository;
 
 import com.yongde.blog.entity.User;
+import org.jspecify.annotations.NullMarked;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@NullMarked
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
